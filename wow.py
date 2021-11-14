539add42ef1104e8bbed96d7ebe2cb05e0e22657
bobby-boy49 <63045905+bobby-boy49@users.noreply.github.com>
2021-11-14 15:43:33 -0500

    Add files via upload

--- wow.py
+++ wow.py
@@ -1,6 +1,12 @@
 # wow.py by bobby_boy49
 
+import time
+
 print ("OMG SUCH WOW!")
+
+print ("your computer has been hacked by your boi raid shadow legends!")
+
+time.sleep(3)
 
 print ("UwU " * 99999999 + "\nshantae so cute!")
 
