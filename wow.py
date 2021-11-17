96db1cde15dccdb7a07b9947f3ce3e391fad2623
bobby-boy49 <63045905+bobby-boy49@users.noreply.github.com>
2021-11-17 03:26:01 +0000

    Update wow.py

--- wow.py
+++ wow.py
@@ -10,7 +10,7 @@ print ("your computer has been hacked by your boi raid shadow legends!")
 
 time.sleep(3)
 
-print ("UwU " * 99999999 + "\n" "shantae so cute!")
+print ("UwU " * 99999999 + "\n" "\nshantae so cute!")
 
 print ("\n")
 
